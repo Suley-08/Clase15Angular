5e80811afc833216487a2d7b5dccb960efdc7a97
Karen Rivera <karenriverah.18@gmail.com>
2023-01-13 01:05:54 -0500

    modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { CreateNewCategoryComponent } from './create-new-category/create-new-category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TransactionsComponent,
+    CreateNewCategoryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  exports: [
+    TransactionsComponent,
+    CreateNewCategoryComponent
+  ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/create-new-category/create-new-category.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-new-category',
+  templateUrl: './create-new-category.component.html',
+  styleUrls: ['./create-new-category.component.scss']
+})
+export class CreateNewCategoryComponent {
+
+}
